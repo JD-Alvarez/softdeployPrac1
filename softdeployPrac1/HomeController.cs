34e39d769da5b4abcdc762aa374f42e9ff3e047e
MATEO GONZALEZ ESCUDERO <mateogonzalez223620@correo.itm.edu.co>
2026-02-23 19:37:28 -0500

    feat(HomeController): Se agrega funcionalidad para emitir mensaje hello World

---
+++ softdeployPrac1/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace softdeployPrac1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok("Hello World desde la nueva funcionalidad");
+        }
+    }
+}
+
